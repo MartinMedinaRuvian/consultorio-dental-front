b1108d952f6ccd3d4364bb0ac2f5221c101f2f3b
MartinMedinaRuvian <martinjesusmr@ufps.edu.co>
2022-04-13 11:11:55 -0500

    Ajuste a disenio de vista de los pacientes

--- src/views/Pacientes/Pacientes.vue
+++ src/views/Pacientes/Pacientes.vue
@@ -2,9 +2,22 @@
 <div class="text-center">
     <Mensaje :mensaje="mensaje" />
     <h3>Pacientes</h3>
-    <div class="d-flex justify-content-center align-items-center container">
-        <input @keypress="verPacientes()" type="text" class="form-control text-center formulario" placeholder="Ingrese el paciente" v-model="filtro">
-        <button class="btn btn-outline-success" @click="verPacientes()"><span class="icon-Lupa"></span></button>
+    <div class="contenedor-filtro_general">
+      <div class="contenedor-filtro">
+        <label for="" class="col-md-12">Seleccione filtro:</label>
+        <select class="form-select form-control col-md-12" aria-label="Default select example">
+          <option value="nombres" selected>Nombres </option>
+          <option value="apellidos">Apellidos</option>
+          <option value="identificacion">Identificación</option> 
+        </select>
+      </div>
+      <div class="contenedor-input">
+        <label for="" class="col-md-12">Buscar:</label>
+        <div class="contenedor-input_buscar">
+          <input @keypress="verPacientes()" type="text" class="form-control text-center formulario" placeholder="Ingrese el dato" v-model="filtro">
+          <button class="btn btn-outline-success ml-2" @click="verPacientes()"><span class="icon-Lupa"></span></button>
+        </div>
+      </div>
     </div>
     <button type="button" class="btn btn-success mt-3" data-toggle="modal" data-target="#categoriaGuardarModal">
         Nuevo
@@ -47,3 +60,17 @@ export default {
     }
 }
 </script>
+<style lang="css">
+  .contenedor-filtro_general {
+    margin-top: 20px;
+    display: flex;
+    align-content: center;
+    justify-content: center;
+  } 
+  .contenedor-input {
+    margin-left: 10px;
+  }
+  .contenedor-input_buscar {
+    display: flex;
+  }
+</style>
